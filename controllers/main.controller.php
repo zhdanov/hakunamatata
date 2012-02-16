2a8ac3c601b6cef1fd8d8b879a04917395f6a940
zhdanov <yuriy.zhdanov@gmail.com>
2012-02-17 00:59:44 +0400

    #3, #4

--- controllers/main.controller.php
+++ controllers/main.controller.php
@@ -1,1 +1,12 @@
 <?php
+namespace Hakunamatata;
+
+require_once(__DIR__ . '/../classes/Autoload.class.php');
+spl_autoload_register(__NAMESPACE__ . '\Autoload::load');
+
+$request = new Request();
+
+if(!$request->uri(0)) {
+  $index = new IndexController();
+  $index->indexAction();
+}
