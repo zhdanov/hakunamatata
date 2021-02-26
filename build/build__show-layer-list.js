bb94a4b73142b3b128191385c612bc79df731006
zhdanov <yuriy.zhdanov@gmail.com>
2021-02-26 20:29:50 +0300

    feat(build): show-layer-list: prefix, ignore filter

--- build/build__show-layer-list.js
+++ build/build__show-layer-list.js
@@ -5,13 +5,25 @@ let list = getLayerList();
 // filter
 if (process.argv[2]) {
   const filter = process.argv[2];
-  list = list.filter(f=>f.indexOf(filter)!=-1);
+  if (filter.substring(0,1) == '!') {
+    if (filter.length > 1) {
+      list = list.filter(f=>f.indexOf(filter.substring(1))==-1);
+    }
+  } else {
+    list = list.filter(f=>f.indexOf(filter)!=-1);
+  }
 }
 
 // postfix
 if (process.argv[3]) {
   const postfix = process.argv[3];
   list = list.map(m=>m+postfix);
+}
+
+// prefix
+if (process.argv[4]) {
+  const prefix = process.argv[4];
+  list = list.map(m=>prefix+m);
 }
 
 console.log(list.join(' '));
